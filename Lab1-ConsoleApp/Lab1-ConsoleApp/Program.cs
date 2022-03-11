720793006f00786f45873feaceb90dd24e853b80
Arthur Dvorak <artdvorak@yandex.ru>
2022-03-11 10:50:04 +0300

    first laboratory

---
+++ Lab1-ConsoleApp/Lab1-ConsoleApp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Lab1_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string textFromFile;
+
+            // создаем каталог для файла
+            string path = @"C:\SortFolder";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+            // чтение из файла
+            using (FileStream fstream = File.OpenRead(@"C:\SortFolder\inputLaba1.txt"))
+            {
+                // преобразуем строку в байты
+                byte[] array = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(array, 0, array.Length);
+                // декодируем байты в строку
+                string text = System.Text.Encoding.Default.GetString(array);
+
+                textFromFile = text;
+
+            }
+
+           
+
+            //запись в файл 
+            using (FileStream fstream = new FileStream(@"C:\SortFolder\outputLaba1.txt", FileMode.Create))
+            {
+                //разбиваем на массив строк 
+                string[] nums = textFromFile.Split(separator: new string[] { "\r", "\n" }, StringSplitOptions.None);
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    // разбиваем строку на массив из 2х элементов
+                    string[] firstAndSecondNumber = nums[i].Split(new char[] { ',' }, StringSplitOptions.None);
+
+                    if (firstAndSecondNumber[0] != "")
+                    {
+
+                        textFromFile = "X:" + firstAndSecondNumber[0].Replace(".",",") + " Y:" + firstAndSecondNumber[1].Replace(".", ",") + "\n";
+
+                        // преобразуем строку в байты
+                        byte[] input = Encoding.Default.GetBytes(textFromFile);
+                        // запись массива байтов в файл
+                        fstream.Write(input, 0, input.Length);
+
+                    }
+                }
+                
+                Console.WriteLine("Текст записан в файл");
+            }
+        }
+    }
+}
---
+++ Lab1-WpfAppNew/Lab1-WpfAppNew/MainWindow.xaml.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System;
+
+namespace Lab1_WpfAppNew
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (UIElement el in MainRoot.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+
+            }
+
+            string textFromFile;
+
+            // создаем каталог для файла
+            string path = @"C:\SortFolder";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+            // чтение из файла
+            using (FileStream fstream = File.OpenRead(@"C:\SortFolder\inputLaba1.txt"))
+            {
+                // преобразуем строку в байты
+                byte[] array = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(array, 0, array.Length);
+                // декодируем байты в строку
+                string text = System.Text.Encoding.Default.GetString(array);
+
+                textFromFile = text;
+
+            }
+
+
+
+            //запись в файл 
+            using (FileStream fstream = new FileStream(@"C:\SortFolder\outputLaba1.txt", FileMode.Create))
+            {
+                //разбиваем на массив строк 
+                string[] nums = textFromFile.Split(separator: new string[] { "\r", "\n" }, StringSplitOptions.None);
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    // разбиваем строку на массив из 2х элементов
+                    string[] firstAndSecondNumber = nums[i].Split(new char[] { ',' }, StringSplitOptions.None);
+
+                    if (firstAndSecondNumber[0] != "")
+                    {
+
+                        textFromFile = "X:" + firstAndSecondNumber[0].Replace(".", ",") + " Y:" + firstAndSecondNumber[1].Replace(".", ",") + "\n";
+
+                        // преобразуем строку в байты
+                        byte[] input = Encoding.Default.GetBytes(textFromFile);
+                        // запись массива байтов в файл
+                        fstream.Write(input, 0, input.Length);
+
+                    }
+                }
+
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            string str = (string)((Button)e.OriginalSource).Content;
+
+            if (str == "AC")
+            {
+                textLabel.Text = "";
+            }
+               
+
+            if (textLabel1.Text != null)
+            {
+                textLabel1.Text = null;
+
+            }
+
+            if (str == "Запись в файл")
+            {
+                string textFromFile;
+                
+                // чтение из файла
+                using (FileStream fstream = File.OpenRead(@"C:\SortFolder\inputLaba1.txt"))
+                {
+                    // преобразуем строку в байты
+                    byte[] array = new byte[fstream.Length];
+                    // считываем данные
+                    fstream.Read(array, 0, array.Length);
+                    // декодируем байты в строку
+                    string text = System.Text.Encoding.Default.GetString(array);
+
+                    textFromFile = text;
+
+                }
+
+                //запись в файл 
+                using (FileStream fstream = new FileStream(@"C:\SortFolder\outputLaba1.txt", FileMode.Create))
+                {
+                    //разбиваем на массив строк 
+                    string[] nums = textFromFile.Split(separator: new string[] { "\r", "\n" }, StringSplitOptions.None);
+
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+                        // разбиваем строку на массив из 2х элементов
+                        string[] firstAndSecondNumber = nums[i].Split(new char[] { ',' }, StringSplitOptions.None);
+
+                        if (firstAndSecondNumber[0] != "")
+                        {
+
+                            textFromFile = "X:" + firstAndSecondNumber[0].Replace(".", ",") + " Y:" + firstAndSecondNumber[1].Replace(".", ",") + "\n";
+
+                            // преобразуем строку в байты
+                            byte[] input = Encoding.Default.GetBytes(textFromFile);
+                            // запись массива байтов в файл
+                            fstream.Write(input, 0, input.Length);
+
+                        }
+                    }
+
+
+                    string[] numsOfInputText = textLabel.Text.Split(new char[] { ',' }, StringSplitOptions.None);
+
+                    if (numsOfInputText[0] != "")
+                    {
+
+                        textFromFile = "X:" + numsOfInputText[0].Replace(".", ",") + " Y:" + numsOfInputText[1].Replace(".", ",") + "\n";
+
+                        // преобразуем строку в байты
+                        byte[] input = Encoding.Default.GetBytes(textFromFile);
+                        // запись массива байтов в файл
+                        fstream.Write(input, 0, input.Length);
+
+                    }
+
+                    textLabel.Text = "";
+                    textLabel1.Text = "Текст записан в файл!";
+
+                }
+
+            }
+
+            else
+            {
+                textLabel.Text += str;
+            }
+                
+
+
+        }
+
+       
+    }
+}
